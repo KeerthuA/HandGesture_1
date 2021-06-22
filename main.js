25b3232f54535c36c4f36780a48c4e0b8c14714b
KeerthuA <77785377+KeerthuA@users.noreply.github.com>
2021-06-22 10:24:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -26,9 +26,54 @@ function modelLoded()
 }
 
 function speak(){
+    console.log("i am in speak function")
     var synth = window.speechSynthesis;
     speak_data_1 = "The Prediction is " + prediction_1;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1);
     synth.speak(utterThis);
 
+}
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error){
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+         prediction_1 = results[0].label;
+        speak();
+        if(results[0].label == "Victory")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#129304;";
+        }
+        if(results[0].label == "Amazing")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        if(results[0].label == "Good")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "clapps")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128079;";
+        }
+        if(results[0].label == "Protest")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9994;";
+            
+        }
+        if(results[0].label == "Stop")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9995;";
+            
+        }
+    }
 }
